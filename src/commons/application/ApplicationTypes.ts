3ffab9638a0b27abb3ea94a1b5e8a23adb84f291
Yu Han Zhang <yuhan@zhyuhan.me>
2023-03-29 22:14:54 +0800

    feat: add submissions table filter state to Redux store

--- src/commons/application/ApplicationTypes.ts
+++ src/commons/application/ApplicationTypes.ts
@@ -293,6 +293,10 @@ export const defaultWorkspaceManager: WorkspaceManagerState = {
   },
   grading: {
     ...createDefaultWorkspace('grading'),
+    submissionsTableFilters: {
+      columnFilters: [],
+      globalFilter: null
+    },
     currentSubmission: undefined,
     currentQuestion: undefined,
     hasUnsavedChanges: false
--- src/commons/workspace/WorkspaceActions.ts
+++ src/commons/workspace/WorkspaceActions.ts
@@ -42,6 +42,7 @@ import {
   SEND_REPL_INPUT_TO_OUTPUT,
   SET_FOLDER_MODE,
   SHIFT_EDITOR_TAB,
+  SubmissionsTableFilters,
   TOGGLE_EDITOR_AUTORUN,
   TOGGLE_FOLDER_MODE,
   TOGGLE_USING_SUBST,
@@ -54,6 +55,7 @@ import {
   UPDATE_HAS_UNSAVED_CHANGES,
   UPDATE_REPL_VALUE,
   UPDATE_SUBLANGUAGE,
+  UPDATE_SUBMISSIONS_TABLE_FILTERS,
   UPDATE_WORKSPACE,
   WorkspaceLocation,
   WorkspaceState
@@ -288,6 +290,9 @@ export const setIsEditorReadonly = (
     workspaceLocation,
     isEditorReadonly: isEditorReadonly
   });
+
+export const updateSubmissionsTableFilters = (filters: SubmissionsTableFilters) =>
+  action(UPDATE_SUBMISSIONS_TABLE_FILTERS, { filters });
 
 export const updateCurrentAssessmentId = (assessmentId: number, questionId: number) =>
   action(UPDATE_CURRENT_ASSESSMENT_ID, {
